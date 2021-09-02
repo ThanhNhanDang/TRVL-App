460c6a81f7d974e90c7eadde7fb5c8622f5f12a7
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-09-02 21:21:23 +0700

    Updates

---
+++ precache-manifest.954124c987e7b2c58a9ec3cccd68e7af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "612e2e7308b0da6fca83",
+    "url": "/TRVL-App/static/css/main.e438e2db.chunk.css"
+  },
+  {
+    "revision": "612e2e7308b0da6fca83",
+    "url": "/TRVL-App/static/js/main.612e2e73.chunk.js"
+  },
+  {
+    "revision": "f9d9f9e39741814c323c",
+    "url": "/TRVL-App/static/js/1.f9d9f9e3.chunk.js"
+  },
+  {
+    "revision": "303502582a76b1fa3928",
+    "url": "/TRVL-App/static/js/runtime~main.30350258.js"
+  },
+  {
+    "revision": "599750e77af151c3ee13ff9a736c25dd",
+    "url": "/TRVL-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b492da53f116552997972d2fb513caf5.js"
+  "/TRVL-App/precache-manifest.954124c987e7b2c58a9ec3cccd68e7af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/TRVL-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
