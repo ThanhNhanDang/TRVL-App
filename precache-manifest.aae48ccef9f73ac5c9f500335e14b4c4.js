f1556d2db78cb044e9b76ecbfe42e077505f5f12
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-09-02 21:05:02 +0700

    Updates

---
+++ precache-manifest.aae48ccef9f73ac5c9f500335e14b4c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "554977232cba37ebd4a8",
+    "url": "/TRVL-App/static/css/main.4ebe69e0.chunk.css"
+  },
+  {
+    "revision": "554977232cba37ebd4a8",
+    "url": "/TRVL-App/static/js/main.55497723.chunk.js"
+  },
+  {
+    "revision": "f9d9f9e39741814c323c",
+    "url": "/TRVL-App/static/js/1.f9d9f9e3.chunk.js"
+  },
+  {
+    "revision": "303502582a76b1fa3928",
+    "url": "/TRVL-App/static/js/runtime~main.30350258.js"
+  },
+  {
+    "revision": "607f2cfdd4911ab62f03a3b1c64a93dc",
+    "url": "/TRVL-App/index.html"
+  }
+];
